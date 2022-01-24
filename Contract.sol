16672272bac0c5ba29481c81595b8a2650dbae1b
crypto-ninja <kotovubalenko.ku@gmail.com>
2022-01-24 12:51:32 +0900

    Chapter 4: Require

--- Contract.sol
+++ Contract.sol
@@ -13,8 +13,12 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
         // and fire it here
         emit NewZombie(id, _name, _dna);
     }
